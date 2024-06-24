0519a99c4bcec7541b2e92bca7de60634bfa6df7
tolerance8274 <tolerance8274@protonmail.com>
2024-06-24 23:52:25 +0300

    fix typo in docs

---
+++ Fly/fly/src/com/xkings/fly/graphics/Shader.java
@@ -0,0 +1,115 @@
+package com.xkings.fly.graphics;
+
+import java.util.HashMap;
+
+import com.badlogic.gdx.Application.ApplicationType;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+
+public class Shader {
+    private static Shader instance;
+
+    private enum ShaderType {
+        vsh,
+        fsh;
+    }
+
+    private static class ShaderStructure {
+        private final String name;
+        private String vertex;
+        private String fragment;
+        private ShaderProgram program;
+
+        public ShaderStructure(String name) {
+            this.name = name;
+        }
+
+        public void setVertex(String vertex) {
+            this.vertex = vertex;
+            compile();
+        }
+
+        public void setFragment(String fragment) {
+            this.fragment = fragment;
+            compile();
+        }
+
+        private void compile() {
+            if (vertex != null && fragment != null) {
+                System.out.println("Compiling shader [" + name + "]");
+                program = new ShaderProgram(vertex, fragment);
+                if (!program.isCompiled()) {
+                    throw new ShaderException(name, program);
+                }
+            }
+        }
+
+        public ShaderProgram getShader() {
+            return program;
+        }
+
+    }
+
+    private final HashMap<String, ShaderStructure> map = new HashMap<String, ShaderStructure>();
+
+    public Shader() {
+        FileHandle dirHandle;
+
+        // Eclipse side-effect of linking assets, assets are copied in bin folder
+        if (Gdx.app.getType() == ApplicationType.Android) {
+            dirHandle = Gdx.files.internal("data/shaders");
+        } else {
+            // ApplicationType.Desktop ..
+            dirHandle = Gdx.files.internal("./bin/data/shaders");
+            if(!dirHandle.exists()){
+                dirHandle = Gdx.files.internal("data/shaders");
+            }
+        }
+
+        FileHandle[] list = dirHandle.list();
+
+        for (FileHandle f : list) {
+
+            String ext = f.extension();
+            String name = f.nameWithoutExtension();
+
+            ShaderStructure ss;
+            if (!map.containsKey(name)) {
+                ss = new ShaderStructure(name);
+            } else {
+                ss = map.get(name);
+            }
+
+            switch (ShaderType.valueOf(ext)) {
+                case vsh:
+                    ss.setVertex(f.readString());
+                    break;
+                case fsh:
+                    ss.setFragment(f.readString());
+                    break;
+            }
+
+            if (!map.containsKey(name)) {
+                map.put(name, ss);
+            }
+
+        }
+    }
+
+    private ShaderStructure getStructure(String name) {
+        ShaderStructure value = map.get(name);
+        if (value == null) {
+            throw new IllegalArgumentException("Specified Shader does not exists: " + name);
+        }
+        return value;
+    }
+
+    public static ShaderProgram getShader(String name) {
+        if (instance == null) {
+            instance = new Shader();
+        }
+        return instance.getStructure(name).getShader();
+    }
+
+}
